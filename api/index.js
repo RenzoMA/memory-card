8411f891930dad69e32be3aadf01919999104d01
Renzo Maldonado <renzoma89@gmail.com>
2023-11-08 17:49:22 -0500

    test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-module.exports = (req, res) => {
-	res.status(200).send('Hello, World!');
+import serverless from 'serverless-http';
+
+module.exports = async (req, res) => {
+    const app = await import('../dist/memory/server/server.mjs');
+	const handler = serverless(app);
+	return handler(req, res);
 };
