dd969a53507d31e0256c7acb84fe3d02946f11d0
Renzo Maldonado <renzoma89@gmail.com>
2023-11-08 16:39:09 -0500

    test

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-module.exports = async () => {
+module.exports = async (req, next) => {
 	const module = await import('../dist/memory/server/server.mjs');
 	const app = module.app;
-	return app();
+	return app(req, next);
 };
