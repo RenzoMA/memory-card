ede2e199257de148675e00d5e0ef0a016cbc25c0
Renzo Maldonado <renzoma89@gmail.com>
2023-11-09 00:56:40 -0500

    first commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 module.exports = async (req, res) => {
 	const serverModule = await import('../dist/memory/server/server.mjs');
-	// Assuming getServer is an async function that returns an Express application
 	const app = await serverModule.getServer();
-
 	return app(req, res);
 };
