c9eb8edd939c7b233a82cd53a26db06525b494fe
Renzo Maldonado <renzoma89@gmail.com>
2023-11-08 22:03:53 -0500

    test

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@
 // Use async function to handle the dynamic import
 module.exports = async () => {
 	// Dynamically import the ES module
-	const server = await import('../dist/memory/server/main.server.mjs');
-    throw new Error(JSON.stringify(server));
+	const { app } = await import('../dist/memory/server/server.mjs');
+
 	// Return the app from the imported module
-	return server.app();
+	return app;
 };
