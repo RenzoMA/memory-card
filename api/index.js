48770f1003aba426740e02cfdd736de40fd387f0
Renzo Maldonado <renzoma89@gmail.com>
2023-11-09 00:53:59 -0500

    testing

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 module.exports = async (req, res) => {
 	const serverModule = await import('../dist/memory/server/server.mjs');
-	const server = serverModule.default || serverModule;
-
 	// Assuming getServer is an async function that returns an Express application
-	const app = await server.getServer();
+	const app = await serverModule.getServer();
 
 	return app(req, res);
 };
