7997aa06abb1b0db486df8648cc8f751db0ba730
Renzo Maldonado <renzoma89@gmail.com>
2023-11-08 21:54:16 -0500

    test

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@
 module.exports = async () => {
 	// Dynamically import the ES module
 	const server = await import('../dist/memory/server/main.server.mjs');
-
+    throw new Error(JSON.stringify(server));
 	// Return the app from the imported module
 	return server.app();
 };
