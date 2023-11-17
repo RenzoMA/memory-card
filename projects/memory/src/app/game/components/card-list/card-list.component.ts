621f09b1726bcde308fe19505ea5c5261c890400
Renzo Maldonado <renzoma89@gmail.com>
2023-11-17 16:13:04 -0500

    refactor into a service

--- projects/memory/src/app/game/components/card-list/card-list.component.ts
+++ projects/memory/src/app/game/components/card-list/card-list.component.ts
@@ -2,9 +2,9 @@ import { AfterViewInit, Component, DestroyRef, Input, QueryList, ViewChildren, i
 import { CommonModule } from '@angular/common';
 import { Card } from '../../models/card.model';
 import { CardComponent } from '../card/card.component';
-import { Subject, bufferCount, concatMap, delay, first, map, merge, of, takeUntil, tap, timer } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { duplicateAndRamdomize } from '../../utils/duplicate-randomize-cards.transform';
+import { GameService } from '../../services/game.service';
 
 @Component({
 	selector: 'app-card-list',
@@ -17,73 +17,19 @@ export class CardListComponent implements AfterViewInit {
 	@Input({ required: true, transform: duplicateAndRamdomize }) cards: Card[] = [];
 
 	@ViewChildren(CardComponent) componentCards!: QueryList<CardComponent>;
-	gameCompleted$ = new Subject<void>();
 
+	gameService = inject(GameService);
 	destroyRef = inject(DestroyRef);
 	elapsedTime = '00:00';
 
 	ngAfterViewInit(): void {
-		const cardSelectionObservables = this.componentCards.map(card => card.selected.asObservable());
-
-		const gameTimer$ = merge(...cardSelectionObservables).pipe(
-			first(),
-			concatMap(() => {
-				return timer(0, 1000);
-			}),
-			takeUntil(this.gameCompleted$)
-		);
-
-		gameTimer$.subscribe(seconds => {
-			this.elapsedTime = this.formatTime(seconds);
-		});
-
-		merge(...cardSelectionObservables)
-			.pipe(
-				bufferCount(2),
-				delay(100),
-				concatMap(([firstComponent, secondComponent]) => {
-					const { card: firstCard } = firstComponent;
-					const { card: secondCard } = secondComponent;
-					if (firstCard.icon !== secondCard.icon) {
-						return of([firstComponent, secondComponent]).pipe(
-							tap(([firstComponent, secondComponent]) => {
-								firstComponent.animateError();
-								secondComponent.animateError();
-							}),
-							delay(500),
-							tap(([firstComponent, secondComponent]) => {
-								firstComponent.hide();
-								secondComponent.hide();
-							})
-						);
-					} else {
-						return of([firstComponent, secondComponent]).pipe(
-							tap(([firstComponent, secondComponent]) => {
-								firstComponent.animateSuccess();
-								secondComponent.animateSuccess();
-							})
-						);
-					}
-				}),
-				tap(() => {
-					const isGameCompleted = this.componentCards.toArray().every(component => !component.isFaceDown);
-					if (isGameCompleted) {
-						this.gameCompleted$.next();
-						this.gameCompleted$.complete();
-					}
-				}),
-				takeUntilDestroyed(this.destroyRef)
-			)
+		this.gameService
+			.initializeGame(this.componentCards.toArray())
+			.pipe(takeUntilDestroyed(this.destroyRef))
 			.subscribe();
-	}
 
-	formatTime(seconds: number): string {
-		const minutes = Math.floor(seconds / 60);
-		const remainingSeconds = seconds % 60;
-
-		const paddedMinutes = minutes.toString().padStart(2, '0');
-		const paddedSeconds = remainingSeconds.toString().padStart(2, '0');
-
-		return `${paddedMinutes}:${paddedSeconds}`;
+		this.gameService.gameTimer$.subscribe(timer => {
+			this.elapsedTime = timer;
+		});
 	}
 }
---
+++ projects/memory/src/app/game/services/game.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@angular/core';
+import {
+	Observable,
+	Subject,
+	bufferCount,
+	concatMap,
+	defer,
+	delay,
+	first,
+	map,
+	merge,
+	of,
+	takeUntil,
+	tap,
+	timer,
+} from 'rxjs';
+import { CardComponent } from '../components/card/card.component';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class GameService {
+	cardComponentSelection: Observable<CardComponent>[] = [];
+	cardComponents: CardComponent[] = [];
+
+  gameCompleted$ = new Subject<void>();
+  
+	gameTimer$ = defer(() => {
+		return merge(...this.cardComponentSelection).pipe(
+			first(),
+			concatMap(() => {
+				return timer(0, 1000);
+			}),
+			map(seconds => {
+				return this.formatTime(seconds);
+			}),
+			takeUntil(this.gameCompleted$)
+		);
+	});
+
+	startGame$ = defer(() => {
+		return merge(...this.cardComponentSelection).pipe(
+			bufferCount(2),
+			delay(100),
+			concatMap(([firstComponent, secondComponent]) => {
+				const { card: firstCard } = firstComponent;
+				const { card: secondCard } = secondComponent;
+				if (firstCard.icon !== secondCard.icon) {
+					return of([firstComponent, secondComponent]).pipe(
+						tap(([firstComponent, secondComponent]) => {
+							firstComponent.animateError();
+							secondComponent.animateError();
+						}),
+						delay(500),
+						tap(([firstComponent, secondComponent]) => {
+							firstComponent.hide();
+							secondComponent.hide();
+						})
+					);
+				} else {
+					return of([firstComponent, secondComponent]).pipe(
+						tap(([firstComponent, secondComponent]) => {
+							firstComponent.animateSuccess();
+							secondComponent.animateSuccess();
+						})
+					);
+				}
+			}),
+			tap(() => {
+				const isGameCompleted = this.cardComponents.every(component => !component.isFaceDown);
+				if (isGameCompleted) {
+					this.gameCompleted$.next();
+					this.gameCompleted$.complete();
+				}
+			})
+		);
+	});
+
+	constructor() {}
+
+	initializeGame(componentCards: CardComponent[]) {
+		this.cardComponents = componentCards;
+		this.cardComponentSelection = componentCards.map(card => card.selected.asObservable());
+		return this.startGame$;
+	}
+
+	formatTime(seconds: number): string {
+		const minutes = Math.floor(seconds / 60);
+		const remainingSeconds = seconds % 60;
+
+		const paddedMinutes = minutes.toString().padStart(2, '0');
+		const paddedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+		return `${paddedMinutes}:${paddedSeconds}`;
+	}
+}
